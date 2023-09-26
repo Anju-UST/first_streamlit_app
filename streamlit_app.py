2cf17d8539d44ff52bec304bf8118472ced78035
Anju Rachel Reji <anjurachelreji51@gmail.com>
2023-09-26 12:28:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,9 @@ streamlit.write('The user entered ', fruit_choice)
 #new section to display fruitwise api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# take the json version and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as table
+streamlit.dataframe(fruityvice_normalized)
+
