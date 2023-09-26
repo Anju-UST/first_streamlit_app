3de5023c5c296022949a03a4aed70a3b7e42493c
Anju Rachel Reji <anjurachelreji51@gmail.com>
2023-09-26 15:55:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,5 +37,10 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+my_cur.execute("select * from fruit_load_list")
+my_data_row= my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
 
 
